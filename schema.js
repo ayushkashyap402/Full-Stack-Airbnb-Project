3c2f173fabb474035b088604599f1f55901ab2af
ayushkashyap402 <ayushkashyap402@gmail.com>
2025-09-22 14:21:21 +0530

    updated

--- schema.js
+++ schema.js
@@ -2,11 +2,11 @@ const Joi = require('joi');
 
 module.exports.listingSchema = Joi.object({
     listing : Joi.object({
-        title: Joi.string().required,
+        title: Joi.string().required(),
         description: Joi.string().required(),
-        price: Joi.number().required,
-        location: Joi.string().required,
-        country: Joi.string().required,
+        price: Joi.number().required(),
+        location: Joi.string().required(),
+        country: Joi.string().required(),
         image: Joi.string().allow("",null)
     }).required(),
 });
