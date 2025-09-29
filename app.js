0aed24ff971bd06ada5048b33aec60ae031affdf
ayushkashyap402 <ayushkashyap402@gmail.com>
2025-09-29 11:31:17 +0530

    updated

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const localStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 
 const listing = require("./routes/listing.js")
@@ -48,6 +51,9 @@ app.get("/", (req,res) => {
 
 app.use(session(sessionOptions));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
