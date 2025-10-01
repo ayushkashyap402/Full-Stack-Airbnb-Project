f0e55f1c9891050cdaa76c2fd3dcc85e1b657624
ayushkashyap402 <ayushkashyap402@gmail.com>
2025-10-01 18:46:32 +0530

    updated

--- init/index.js
+++ init/index.js
@@ -16,6 +16,10 @@ async function main() {
 
 const initDB = async () => {
    await Listing.deleteMany({});
+   initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "68da3fe967070a803328e2cf",
+   }));
    await Listing.insertMany(initData.data);
    console.log("data was initailized");
 };
