77c027428d6b45eea8fae3f5907405fd9f6cf552
ayushkashyap402 <ayushkashyap402@gmail.com>
2025-10-06 14:48:18 +0530

    updated

--- routes/user.js
+++ routes/user.js
@@ -5,56 +5,35 @@ const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 
-router.get("/signup",(req, res) => {
-    res.render("users/signup.ejs");
-});
 
+const userController = require("../controllers/users.js");
+const user = require("../models/user.js");
+
+// Render Signup Form
+router.get("/signup",userController.renderSignupForm);
+
+
+// Signup Route
 router.post("/signup", 
-    wrapAsync(async(req ,res) => {
-  try{
-    let {username , email ,password} = req.body;
-    const newUser = new User ({email, username});
-    const registeredUser = await User.register(newUser, password);
-    console.log(registeredUser);
-    req.login(registeredUser,(err) => {
-      if(err) {
-        return next(err);
-      }
-      req.flash("success","Welcome to Explorer");
-      res.redirect("/listings");
-    });
- } catch(e){
-       req.flash("error", e.message);
-       res.redirect("/signup");
-  }
-})
-);
+    wrapAsync(userController.signup));
 
-router.get("/login", (req,res) => {
-    res.render("users/login.ejs");
-})
 
+// Render Login Form
+router.get("/login", userController.renderLoginForm);
+
+
+// Passport Authentication Login 
 router.post("/login",
   saveRedirectUrl,
   passport.authenticate("local",{ 
     failureRedirect: '/login', 
     failureFlash: true
 }),
-async(req, res) => {
-    req.flash("success","Welcome back to Explorer!");
-    let redirectUrl = res.locals.redirectUrl || "/listings";
-    res.redirect(redirectUrl);
-}
+userController.login
 );
 
-router.get("/logout",(req, res, next) => {
-    req.logout((err) => {
-     if(err) {
-        return next(err);
-     }
-     req.flash("success","you are logged out!");
-     res.redirect("/listings");
-    });
-});
+
+// Logout 
+router.get("/logout",userController.logout);
 
 module.exports = router;
