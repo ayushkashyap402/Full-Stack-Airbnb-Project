bef73e08b2ffad5a4e1b0c5648cdba0c281adb8c
ayushkashyap402 <ayushkashyap402@gmail.com>
2025-10-09 16:56:49 +0530

    updated

--- public/js/map.js
+++ public/js/map.js
@@ -1,11 +1,14 @@
  mapboxgl.accessToken = mapToken;
 
+const mapDiv = document.getElementById('map');
+const coordinates = JSON.parse(mapDiv.dataset.coordinates);
+
     const map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v12', // Use the standard style for the map
         // projection: 'globe', // display the map as a globe
         zoom: 9, // initial zoom level, 0 is the world view, higher values zoom in
-        center: [77.2090, 28.6139] // center the map on this longitude and latitude
+        center: coordinates,// center the map on this longitude and latitude
     });
 
     map.addControl(new mapboxgl.NavigationControl());
@@ -14,3 +17,9 @@
     map.on('style.load', () => {
         map.setFog({}); // Set the default atmosphere style
     });
+
+
+
+    const marker = new mapboxgl.Marker()
+    .setLngLat(coordinates) //Listing.geometry.coordinates
+    .addTo(map);
