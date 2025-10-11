37394344e13538c9068e17a6481ff2b86745b1ae
ayushkashyap402 <ayushkashyap402@gmail.com>
2025-10-11 09:25:30 +0530

    updated

--- public/js/map.js
+++ public/js/map.js
@@ -1,14 +1,14 @@
  mapboxgl.accessToken = mapToken;
 
-const mapDiv = document.getElementById('map');
-const coordinates = JSON.parse(mapDiv.dataset.coordinates);
+// const mapDiv = document.getElementById('map');
+// const coordinates = JSON.parse(mapDiv.dataset.coordinates);
 
     const map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v12', // Use the standard style for the map
         // projection: 'globe', // display the map as a globe
         zoom: 9, // initial zoom level, 0 is the world view, higher values zoom in
-        center: coordinates,// center the map on this longitude and latitude
+        center: listing.geometry.coordinates,// center the map on this longitude and latitude
     });
 
     map.addControl(new mapboxgl.NavigationControl());
@@ -20,6 +20,9 @@ const coordinates = JSON.parse(mapDiv.dataset.coordinates);
 
 
 
-    const marker = new mapboxgl.Marker()
-    .setLngLat(coordinates) //Listing.geometry.coordinates
-    .addTo(map);
+    const marker = new mapboxgl.Marker({ color: "red"})
+     .setLngLat(listing.geometry.coordinates) //Listing.geometry.coordinates
+     .setPopup(new mapboxgl.Popup({offset : 25}).setHTML(
+        `<h4>${listing.location}</h4>
+        <p>Exact Location will be provided after booking</p>`))
+     .addTo(map);
